47050564786801fb21d6c671c012890af38a723a
Divyanshu Singh <144301463+DivyanshuSingh1423@users.noreply.github.com>
2024-07-25 18:25:36 +0530

    Update ListProduct.jsx

--- Add-Product/src/Components/ListProduct/ListProduct.jsx
+++ Add-Product/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://backend-p6ur.onrender.com') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
@@ -16,7 +16,7 @@ const ListProduct = () => {
     }, [])
 
     const removeProduct = async (id) => {
-      await fetch('http://localhost:4000/removeproduct', {
+      await fetch('https://backend-p6ur.onrender.com', {
       method: 'POST',
       headers: {
         Accept:'application/json',
@@ -25,7 +25,7 @@ const ListProduct = () => {
       body: JSON.stringify({id:id}),
     })
 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://backend-p6ur.onrender.com') 
     .then((res) => res.json()) 
     .then((data) => setAllProducts(data))
 
