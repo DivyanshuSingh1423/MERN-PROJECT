be0012a539d0aa99e47e29b9c7bac72828b14d18
Divyanshu Singh <144301463+DivyanshuSingh1423@users.noreply.github.com>
2024-07-25 18:32:52 +0530

    Update ShopCategory.jsx

--- Frontend/src/Pages/ShopCategory.jsx
+++ Frontend/src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://backend-p6ur.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
